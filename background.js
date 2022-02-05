0713ee37dffb25de1eea5431bf074b9d8fb51ceb
Ellen Li <x.ellen@gmail.com>
2022-02-04 20:22:13 -0500

    quoteshot.html and basic CSS

--- background.js
+++ background.js
@@ -12,19 +12,20 @@ browser.contextMenus.create({
     id: REQUEST_SELECTION,
     title: "Take Quoteshot", // future: Take Quoteshot of “selected text goes here”
     contexts: ["selection"],
-    onclick: handleCopyRequest
+    onclick: handleQuoteshotRequest
 });
 
-function handleCopyRequest(info, tab) {
+function handleQuoteshotRequest(info, tab) {
     // check in which context the command was clicked
+
+    var newURL = 'quoteshot.html';
+    chrome.tabs.create({ url: newURL });
+
+
     const useSelection = info.menuItemId === REQUEST_SELECTION;
     chrome.tabs.sendMessage(tab.id,
         {tabUrl: tab.url,
          tabTitle: tab.title,
          useSelection: useSelection
     });
-
-    var newURL = 'quoteshot.html'; // how to pass through the quote selection???
-    chrome.tabs.create({ url: newURL });
-
 }
--- quoteshot.js
+++ quoteshot.js
@@ -2,3 +2,15 @@
 // save last line as url, then cut it off...  show it somewhere..
 
 // take remaining text and put it into ... this?: https://github.com/hemanth/make-quote
+
+$(function () {
+  $('.authorToggle').change(function () {
+     $('#author').toggle(!this.checked);
+  }).change(); //ensure visible state matches initially
+});
+
+$(function () {
+  $('.titleToggle').change(function () {
+     $('#title').toggle(!this.checked);
+  }).change(); //ensure visible state matches initially
+});
