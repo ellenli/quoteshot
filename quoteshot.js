e8e8ef7271ecbd8ae85e4dddfe409dda073b54a5
Ellen Li <x.ellen@gmail.com>
2022-02-07 13:00:41 -0500

    move all JS to quoteshot.js

--- quoteshot.js
+++ quoteshot.js
@@ -27,3 +27,57 @@ $(() => {
     }).change()
   })
 })
+
+// save image locally
+function capture() {
+  const captureElement = document.querySelector('.quoteshot-container')
+  html2canvas(captureElement)
+    .then(canvas => {
+      canvas.style.display = 'none'
+      document.body.appendChild(canvas)
+      return canvas
+    })
+    .then(canvas => {
+      const image = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream')
+      const a = document.createElement('a')
+      const filename = 'quoteshot-' + Date.now() + '.png'
+      a.setAttribute('download', filename)
+      a.setAttribute('href', image)
+      a.click()
+      window.alert(filename + " downloaded.")
+      canvas.remove()
+    })
+}
+const download = document.querySelector('#downloadButton')
+download.addEventListener('click', capture)
+
+
+// copy image to clipboard
+function copyToClipboard() {
+  var domNode = document.getElementById('capture');
+  domNode.classList.add("on");
+
+  // reference: https://codepen.io/karannagupta/pen/RXpddB
+  const captureElement = document.getElementById('capture');
+  html2canvas(captureElement).then(function(canvas) (
+    canvas.toBlob(function(blob) {
+        navigator.clipboard
+          .write([
+          new ClipboardItem(
+            Object.defineProperty({}, blob.type, {
+              value: blob,
+              enumerable: true
+            })
+          )
+        ])
+          .then(function() {
+            window.alert("Quoteshot image copied to clipboard.");
+            domNode.classList.remove("on");
+        });
+      })
+    );
+  window.alert("Quoteshot image copied to clipboard.");
+  domNode.classList.remove("on");
+}
+const copy = document.querySelector('#copyButton');
+copy.addEventListener('click', copyToClipboard);
