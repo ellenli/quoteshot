0535e6916e023e5231b48d54d63d09b2818d4931
David Wolever <wolever@sheertex.com>
2022-02-08 13:36:49 -0500

    Update to manifest v3

--- background.js
+++ background.js
@@ -1,7 +1,12 @@
 // Generates a context menu
 // Select text, right click, and ... take a quoteshot???
 
-const REQUEST_SELECTION = 'copy-selection-url-to-clipboard';
+
+function urlencode(obj) {
+  return Object.entries(obj).map(kv => {
+    return encodeURIComponent(kv[0]) + '=' + encodeURIComponent(kv[1])
+  }).join('&')
+}
 
 // support Firefox and Chrome
 if (!browser) {
@@ -9,38 +14,33 @@ if (!browser) {
 }
 
 browser.contextMenus.create({
-    id: REQUEST_SELECTION,
-    title: "Take Quoteshot", // future: Take Quoteshot of “selected text goes here”
-    contexts: ["selection"],
-    onclick: handleQuoteshotRequest
-});
+  id: "quoteshot-take",
+  title: "Take Quoteshot",
+  contexts: ["selection"],
+})
 
-function urlencode(obj) {
-    return Object.entries(obj).map(kv => {
-        return encodeURIComponent(kv[0]) + '=' + encodeURIComponent(kv[1])
-    }).join('&')
-}
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId != "quoteshot-take")
+    return
+  // check in which context the command was clicked
+  const code = `
+    (() => {
+      const authorElem = document.querySelector('meta[name=author]')
+      const author = (authorElem && authorElem.getAttribute('content')) || ''
+      const selection = window.getSelection().toString()
+      return [author, selection]
+    })()
+  `
 
-function handleQuoteshotRequest(info, tab) {
-    // check in which context the command was clicked
-    const code = `
-        (() => {
-            const authorElem = document.querySelector('meta[name=author]')
-            const author = (authorElem && authorElem.getAttribute('content')) || ''
-            const selection = window.getSelection().toString()
-            return [author, selection]
-        })()
-    `
-
-    chrome.tabs.executeScript(tab.id, { code }, result => {
-        const [ author, selection ] = result[0]
-        const newTabReq = chrome.tabs.create({
-            url: 'http://localhost:8000/quoteshot.html?' + urlencode({
-                url: tab.url,
-                title: tab.title,
-                author: author,
-                quote: selection,
-            })
-        })
+  browser.tabs.executeScript(tab.id, { code }, result => {
+    const [ author, selection ] = result[0]
+    const newTabReq = browser.tabs.create({
+      url: 'http://localhost:8000/quoteshot.html?' + urlencode({
+        url: tab.url,
+        title: tab.title,
+        author: author,
+        quote: selection,
+      })
     })
-}
+  })
+});
