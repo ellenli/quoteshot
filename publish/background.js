6e4bbd81aa1c4fa997eeeca0836a1b96e64c4949
Ellen Li <x.ellen@gmail.com>
2022-02-08 22:31:10 -0500

    quoteshot 1.0.0 SUBMITTED and font-picker.html for picker UI development

--- publish/background.js
+++ publish/background.js
@@ -1,46 +1,47 @@
 // Generates a context menu
 // Select text, right click, and ... take a quoteshot???
 
-const REQUEST_SELECTION = 'copy-selection-url-to-clipboard';
+
+function urlencode(obj) {
+  return Object.entries(obj).map(kv => {
+    return encodeURIComponent(kv[0]) + '=' + encodeURIComponent(kv[1])
+  }).join('&')
+}
 
 // support Firefox and Chrome
 if (!browser) {
   var browser = chrome;
 }
 
-browser.contextMenus.create({
-    id: REQUEST_SELECTION,
-    title: "Take Quoteshot", // future: Take Quoteshot of “selected text goes here”
+browser.contextMenus.removeAll(() => {
+  browser.contextMenus.create({
+    id: "quoteshot-take",
+    title: "Take Quoteshot",
     contexts: ["selection"],
-    onclick: handleQuoteshotRequest
-});
+  })
+})
 
-function urlencode(obj) {
-    return Object.entries(obj).map(kv => {
-        return encodeURIComponent(kv[0]) + '=' + encodeURIComponent(kv[1])
-    }).join('&')
-}
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId != "quoteshot-take")
+    return
 
-function handleQuoteshotRequest(info, tab) {
-    // check in which context the command was clicked
-    const code = `
-        (() => {
-            const authorElem = document.querySelector('meta[name=author]')
-            const author = (authorElem && authorElem.getAttribute('content')) || ''
-            const selection = window.getSelection().toString()
-            return [author, selection]
-        })()
-    `
-
-    chrome.tabs.executeScript(tab.id, { code }, result => {
-        const [ author, selection ] = result[0]
-        const newTabReq = chrome.tabs.create({
-            url: 'http://localhost:8000/quoteshot.html?' + urlencode({
-                url: tab.url,
-                title: tab.title,
-                author: author,
-                quote: selection,
-            })
-        })
+  browser.scripting.executeScript({
+    target: {tabId: tab.id},
+    func: () => {
+      const authorElem = document.querySelector('meta[name=author]')
+      const author = (authorElem && authorElem.getAttribute('content')) || ''
+      const selection = window.getSelection().toString()
+      return [author, selection]
+    },
+  }, results => {
+    const [ author, selection ] = results[0].result
+    const newTabReq = browser.tabs.create({
+      url: 'http://localhost:8000/quoteshot.html?' + urlencode({
+        url: tab.url,
+        title: tab.title,
+        author: author,
+        quote: selection,
+      })
     })
-}
+  })
+});
